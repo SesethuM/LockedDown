45a8e59fa9930cbdfa58d5d9541b47033914e4f5
SesethuM <msesethu06@gmail.com>
2024-06-03 14:11:51 +0200

    Webpage Prototype

---
+++ src/main/java/virtualproject/LockedUpDriver.java
@@ -0,0 +1,16 @@
+package virtualproject;
+
+public class LockedUpDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedUp Management", "Liquid Developers");
+		
+		HandleOptions.handleWelcomeScreenInput();
+}
+
+}
